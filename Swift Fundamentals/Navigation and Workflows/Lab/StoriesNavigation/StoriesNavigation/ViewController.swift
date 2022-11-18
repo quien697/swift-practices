a36cf91a7cf245e9f013aade0558cb923517af2c
Quien <quien697@gmail.com>
2022-11-18 13:01:35 -0800

    feat: Add StoriesNavigation of Lab in Navigation and Workflows

---
+++ Swift Fundamentals/Navigation and Workflows/Lab/StoriesNavigation/StoriesNavigation/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  StoriesNavigation
+//
+//  Created by Quien on 2022/11/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+  @IBAction func unwindToDoorway(unwindSegus: UIStoryboardSegue) {
+    
+  }
+}
+
