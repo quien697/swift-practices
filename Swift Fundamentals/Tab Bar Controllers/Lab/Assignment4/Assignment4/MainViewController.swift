55f87e6e3bed637cbee211eadee83f75e4634cdf
Quien <quien697@gmail.com>
2022-11-20 17:00:24 -0800

    feat: Add Assignment4 of Lab in Tab bar Controllers

---
+++ Swift Fundamentals/Tab Bar Controllers/Lab/Assignment4/Assignment4/MainViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MainViewController.swift
+//  Assignment4
+//
+//  Created by Quien on 2022/11/20.
+//  Copyright © 2022 Derrick Park. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
