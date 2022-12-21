f196664d7ccae2a9c6a6a2cb2e915e6723da4150
Quien <quien697@gmail.com>
2022-12-20 17:19:32 -0800

    feat: Add AppLifeCycle of Project in App Anatomy Life Cycle

---
+++ Swift Data Collections/Unit 1/App Anatomy and Life Cycle/AppLifeCycle/AppLifeCycle/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppLifeCycle
+//
+//  Created by Quien on 2022/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+    print(#function)
+  }
+
+
+}
+
