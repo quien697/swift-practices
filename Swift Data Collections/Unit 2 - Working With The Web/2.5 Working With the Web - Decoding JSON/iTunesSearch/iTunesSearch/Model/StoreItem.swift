588458b2be2a3c50533e535395d05871a0147080
Quien <quien697@gmail.com>
2023-01-09 16:48:11 -0800

    feat: done a iTunesSearch of project in the 2.5 Working With The Web - Decoding JSON

---
+++ Swift Data Collections/Unit 2 - Working With The Web/2.5 Working With the Web - Decoding JSON/iTunesSearch/iTunesSearch/Model/StoreItem.swift
@@ -0,0 +1,47 @@
+//
+//  StoreItem.swift
+//  iTunesSearch
+//
+//  Created by Quien on 2023/1/9.
+//
+
+import Foundation
+
+struct StoreItem: Codable {
+  var name: String
+  var artist: String
+  var kind: String
+  var artworkURL: URL
+  var description: String
+  
+  enum CodingKeys: String, CodingKey {
+    case name = "trackName"
+    case artist = "artistName"
+    case kind
+    case artworkURL = "artworkUrl30"
+    case description
+  }
+  
+  enum AdditionalKeys: String, CodingKey {
+    case longDescription
+  }
+
+  init(from decoder: Decoder) throws {
+    let container = try decoder.container(keyedBy: CodingKeys.self)
+    self.name = try container.decode(String.self, forKey: CodingKeys.name)
+    self.artist = try container.decode(String.self, forKey: CodingKeys.artist)
+    self.kind = try container.decode(String.self, forKey: CodingKeys.kind)
+    self.artworkURL = try container.decode(URL.self, forKey: CodingKeys.artworkURL)
+    
+    if let description = try? container.decode(String.self, forKey: CodingKeys.description) {
+      self.description = description
+    } else {
+      let additionalValues = try decoder.container(keyedBy: AdditionalKeys.self)
+      self.description = (try? additionalValues.decode(String.self, forKey: AdditionalKeys.longDescription)) ?? ""
+    }
+  }
+}
+
+enum StoreItemError: Error, LocalizedError {
+  case itemsNotFound
+}
--- Swift Data Collections/Unit 2 - Working With The Web/2.5 Working With the Web - Decoding JSON/iTunesSearch/iTunesSearch/ViewController.swift
+++ Swift Data Collections/Unit 2 - Working With The Web/2.5 Working With the Web - Decoding JSON/iTunesSearch/iTunesSearch/ViewController.swift
@@ -9,25 +9,46 @@ import UIKit
 
 class ViewController: UIViewController {
   
+  let query = [
+    "term": "Brave",
+    "media": "music",
+    "limit": "10"
+  ]
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     
-    var urlComponents = URLComponents(string: "https://itunes.apple.com/search")!
-    let query: [String: String] = [
-      "term": "Brave",
-      "media": "music"]
-    urlComponents.queryItems = query.map { URLQueryItem(name: $0.key, value: $0.value) }
-    
     Task {
-      let (data, response) = try await URLSession.shared.data(from: urlComponents.url!)
-      
-      if let httpResponse = response as? HTTPURLResponse,
-         httpResponse.statusCode == 200,
-         let string = String(data: data, encoding: .utf8) {
-        print(string)
+      do {
+        let storeItems = try await fetchItems(matching: query)
+        storeItems.forEach { item in
+          print("""
+                Name: \(item.name)
+                Artist: \(item.artist)
+                Kind: \(item.kind)
+                Description: \(item.description)
+                Artwork URL: \(item.artworkURL)
+                """)
+        }
+      } catch {
+        print(error)
       }
     }
   }
   
+  func fetchItems(matching query: [String: String]) async throws -> [StoreItem] {
+    var urlComponents = URLComponents(string: "https://itunes.apple.com/search")!
+    urlComponents.queryItems = query.map { URLQueryItem(name: $0.key, value: $0.value) }
+    let (data, response) = try await URLSession.shared.data(from: urlComponents.url!)
+    
+    guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
+      throw StoreItemError.itemsNotFound
+    }
+    
+    let decoder = JSONDecoder()
+    let searchResponse = try decoder.decode(SearchResponse.self, from: data)
+    
+    return searchResponse.results
+  }
+  
 }
-
