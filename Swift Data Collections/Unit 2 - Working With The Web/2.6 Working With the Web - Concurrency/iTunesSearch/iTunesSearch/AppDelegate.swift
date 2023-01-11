7f0a051c93a9c6eb8c069fdcb39e203ef759b0ab
Quien <quien697@gmail.com>
2023-01-10 19:31:00 -0800

    feat: add a features about cache in iTunesSearch of project

--- Swift Data Collections/Unit 2 - Working With The Web/2.6 Working With the Web - Concurrency/iTunesSearch/iTunesSearch/AppDelegate.swift
+++ Swift Data Collections/Unit 2 - Working With The Web/2.6 Working With the Web - Concurrency/iTunesSearch/iTunesSearch/AppDelegate.swift
@@ -14,6 +14,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
     // Override point for customization after application launch.
+    
+    // Set up cache
+    URLCache.shared.memoryCapacity = 25_000_000
+    URLCache.shared.diskCapacity = 50_000_000
     return true
   }
 
