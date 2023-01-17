0eae41b000316c88efe97cca4e6849a1ccad2c07
Quien <quien697@gmail.com>
2023-01-16 19:55:26 -0800

    add a BasicCollectionView of project in the 3.3 Dynamic Data

---
+++ Swift Data Collections/Unit 3 - Advanced Data Display/3.3 Dynamic Data/BasicCollectionView/BasicCollectionView/BasicHeaderView.swift
@@ -0,0 +1,12 @@
+//
+//  BasicHeaderView.swift
+//  BasicCollectionView
+//
+//  Created by Quien on 2023/1/16.
+//
+
+import UIKit
+
+class BasicHeaderView: UICollectionReusableView {
+  @IBOutlet weak var label: UILabel!
+}
---
+++ Swift Data Collections/Unit 3 - Advanced Data Display/3.3 Dynamic Data/BasicCollectionView/README.md
@@ -0,0 +1,6 @@
+#  Basic Collection View
+
+- Book: Develop in Swift Data Collections
+- Unit: 3 - Advanced Data Display
+- Lesson: 3.3 Dynamic Data
+- Page: 611-624
